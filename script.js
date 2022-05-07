c7e7dea555dbbf35d6e6380854c15cbdc93e1084
john Doe <sataward666@gmail.com>
2022-05-07 18:07:05 +0200

    modif send mail with toast & opacity modal

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-const message = "Merci pour votre message. Nous vous répondrons rapidement.";
+/*const message = "Merci pour votre message. Nous vous répondrons rapidement.";
 
 const contactForm = document.getElementById("contactForm");
 
 contactForm.addEventListener("submit", function(event){
   event.preventDefault();
     alert(message);
-});
+}); */
 
-//document
-//  .getElementById("contactForm")
-//  .addEventListener("submit", function (event) {
-//    event.preventDefault();
-//    alert(message);
-//  });
-
+        //****notif burger au click envoyer du formulaire****
+//je sélectionne et stocke la div container-notifications
+const ctn = document.querySelector(".container-notifications");
+//console.log(ctn);
+contactForm.addEventListener("submit", function(event){
+    console.log("bouton cliqué");
+    event.preventDefault(); //stop le rafraichissemment de la page
+    //je crée un élément
+const notification = document.createElement("div");
+//console.log(notification);
+//je donne un style à la div
+notification.classList.add("toast");
+//j'insère du contenu texte
+notification.innerText = "Votre Mail est envoyé !"
+//console.log(notification);
+//j'accroche notification à l'élément ctn
+ctn.appendChild(notification);
+    //A la fin du chrono, je retire la notification du dom
+    setTimeout(function(){
+    notification.remove();
+}, 2000);
+}); //fermeture de btn.addEventListener
+                    //****modal****
   // Get the modal
 const modal = document.getElementById('myModal');
-
 // Get the button that opens the modal
 let btn = document.getElementById("myBtn");
 
@@ -27,12 +42,10 @@ let span = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
     modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
